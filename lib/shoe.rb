8eb76b210f5f4fc7a73366b9d0d8c2b611851371
Sha-jay Perry <shajayjanae@gmail.com>
2020-05-24 00:08:47 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,9 +4,12 @@ class Shoe
   
   BRANDS = [ ]
 
-  def initialize(brand)
+   def initialize(brand)
     @brand = brand
-    BRANDS << brand
+    if BRANDS.include?(brand)
+    else
+      BRANDS << brand
+    end
   end
 
   def cobble
